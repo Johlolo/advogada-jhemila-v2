072f720c81d7036a0fb50efc45280347baa487f1
Johannes Pablo Rey <johannespablorey@gmail.com>
2022-04-06 19:48:16 -0300

    Começo do projeto do Site

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'a55f0267589da9cb7fa87c7b611e9ce52cd29da8',
         'name' => 'johlolo/ecommerce',
         'dev' => true,
     ),
     'versions' => array(
         'johlolo/ecommerce' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'a55f0267589da9cb7fa87c7b611e9ce52cd29da8',
             'dev_requirement' => false,
         ),
         'phpmailer/phpmailer' => array(
